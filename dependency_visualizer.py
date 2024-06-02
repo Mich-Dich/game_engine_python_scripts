bc303dbe6f1af4074e2fb27f84ea88022d860770
Mich-Dich <mally.dominik@gmx.de>
2024-06-02 10:54:26 +0200

    update graph generator

--- dependency_visualizer.py
+++ dependency_visualizer.py
@@ -1,29 +1,34 @@
 import os
 import re
+from collections import defaultdict
 
 def count_lines_of_code(directory):
-    in_block_comment = False
-
-    log_patterns = [r'\bCORE_LOG\b', r'\bLOG\b']
-    log_regex = re.compile('|'.join(log_patterns))
+    include_regex = re.compile(r'#include\s*["<](.*?)[">]')
+    dependency_dict = defaultdict(list)
 
     for root, dirs, files in os.walk(directory):
         # Skip any 'vendor' directories
         dirs[:] = [d for d in dirs if d != 'vendor']
         for file in files:
-            if file.endswith('.cpp') or file.endswith('.h'):
+            if file.endswith('.h'):
                 try:
                     with open(os.path.join(root, file), 'r', encoding='latin-1') as f:
                         for line in f:
-
                             line = line.strip()
                             if line.startswith("#include"):
-                                print(f"line: {line}")
-                    
+                                match = include_regex.search(line)
+                                if match:
+                                    file_path = match.group(1)
+                                    included_file_name_with_ext = os.path.basename(file_path)
+                                    included_file_name, _ = os.path.splitext(included_file_name_with_ext)
+                                    file_name, _ = os.path.splitext(file)
+                                    
+                                    # Add to the dependency dictionary
+                                    dependency_dict[included_file_name].append(file_name)
                 except Exception as e:
                     print(f"Skipping file {file} due to error: {e}")
-    
-    return
+
+    return dependency_dict
 
 def find_projects(directory):
     projects = []
@@ -36,6 +41,16 @@ def find_projects(directory):
                 break
     return projects
 
+def build_tree(dependency_dict, root, indent=""):
+    print(f"{indent}{root}")
+
+    if indent == "":
+        indent = " └─ "
+
+    if root in dependency_dict:
+        for dep in dependency_dict[root]:
+            build_tree(dependency_dict, dep, "  " + indent)
+
 if __name__ == "__main__":
     directory = input("Enter the path to the main directory: ")
     projects = find_projects(directory)
@@ -44,5 +59,10 @@ if __name__ == "__main__":
     
         print()
         print(f"---- Project '{project_name}' ---- ")
-        count_lines_of_code(project)
+        dependency_dict = count_lines_of_code(project)
+        
+        # Print the tree-graph representation
+        for root in dependency_dict:
+            if all(root not in deps for deps in dependency_dict.values()):
+                build_tree(dependency_dict, root)
         print()
